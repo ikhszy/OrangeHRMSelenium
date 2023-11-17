69a0d85ab79fb04e11b1c594cbf2545cc50b74f0
Acer <Acer@DESKTOP-Q9R4LGA>
2023-11-17 18:00:11 +0700

    add more test to admin page

--- src/main/java/org/selenium/orangeHRM/pageObjects/AdminPage.java
+++ src/main/java/org/selenium/orangeHRM/pageObjects/AdminPage.java
@@ -3,11 +3,11 @@ package org.selenium.orangeHRM.pageObjects;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 import org.selenium.orangeHRM.utils.ReusedFeature;
 
 public class AdminPage extends ReusedFeature {
@@ -23,14 +23,17 @@ public class AdminPage extends ReusedFeature {
 	@FindBy(css = ".oxd-input.oxd-input--active")
 	private List <WebElement> searchUsername;
 	
-	@FindBy(className = "oxd-autocomplete-text-input")
+	@FindBy(xpath = "//*[@placeholder='Type for hints...']")
 	private WebElement searchEmployeeName;
 	
-	@FindBy(className = "oxd-autocomplete-dropdown")
-	private List<WebElement> searchEmpNameSuggestion;
+	@FindBy(css = ".oxd-autocomplete-dropdown")
+	private WebElement searchEmpNameSuggestion;
 	
 	@FindBy(className = "oxd-select-text-input")
 	private List <WebElement> selectDropdown;
+	
+	@FindBy(xpath="//*[role='option']")
+	private List <WebElement> dropdownSelections;
 	
 	@FindBy(xpath = "//*[@type='submit']")
 	private WebElement searchButton;
@@ -41,6 +44,9 @@ public class AdminPage extends ReusedFeature {
 	@FindBy(className = "oxd-table-card")
 	private List <WebElement> tableResults;
 	
+	@FindBy(xpath = "//*[@class='oxd-table-cell oxd-padding-cell']")
+	private List <WebElement> tableData; 
+	
 	public void searchUsernameType(String uname) {
 		searchUsername.get(1).sendKeys(uname);
 	}
@@ -50,31 +56,42 @@ public class AdminPage extends ReusedFeature {
 	}
 	
 	public void dropdownRole(String role) {
-		Select sel = new Select(selectDropdown.get(0));
+		selectDropdown.get(0).click();
 		
 		if(role.equalsIgnoreCase("admin")) {
-			sel.selectByVisibleText("Admin");
+			selectDropdown.get(0).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(0).sendKeys(Keys.ENTER);
 		} else {
-			sel.selectByVisibleText("ESS");
+			selectDropdown.get(0).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(0).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(0).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(0).sendKeys(Keys.ENTER);
 		}
 	}
 	
 	public void dropdownStatus(String status) {
-		
-		Select sel = new Select(selectDropdown.get(1));
-		
+		selectDropdown.get(1).click();
 		if(status.equalsIgnoreCase("enabled")) {
-			sel.selectByVisibleText("Enabled");
+			selectDropdown.get(1).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(1).sendKeys(Keys.ENTER);
 		} else {
-			sel.selectByVisibleText("Disabled");
+			selectDropdown.get(1).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(1).sendKeys(Keys.ARROW_DOWN);
+			selectDropdown.get(1).sendKeys(Keys.ENTER);
 		}
 	}
 	
 	public void searchEmployeeName(String emp) {
 		searchEmployeeName.sendKeys(emp);
 		
-		waitForElementToAppear(By.className("oxd-autocomplete-dropdown"));
-		searchEmpNameSuggestion.get(0).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		searchEmpNameSuggestion.click();
 	}
 	
 	public void searchClick() {
@@ -89,6 +106,46 @@ public class AdminPage extends ReusedFeature {
 	public int searchResultTable() {
 		int table = tableResults.size();
 		return table;
+	}
+	
+	public String getTableUsername(int index) {
+		String userName = "";
+		if(index == 0) {
+			userName = tableData.get(index + 1).getText();
+		} else {
+			userName = tableData.get(index * 6 + 1).getText();
+		}
+		return userName;
+	}
+	
+	public String getTableUserRole(int index) {
+		String userRole = "";
+		if(index == 0) {
+			userRole = tableData.get(index + 2).getText();
+		} else {
+			userRole = tableData.get(index * 6 + 2).getText();
+		}
+		return userRole;
+	}
+	
+	public String getTableEmployeename(int index) {
+		String userRole = "";
+		if(index == 0) {
+			userRole = tableData.get(index + 3).getText();
+		} else {
+			userRole = tableData.get(index * 6 + 3).getText();
+		}
+		return userRole;
+	}
+	
+	public String getTableUserStatus(int index) {
+		String userRole = "";
+		if(index == 0) {
+			userRole = tableData.get(index + 4).getText();
+		} else {
+			userRole = tableData.get(index * 6 + 4).getText();
+		}
+		return userRole;
 	}
 
 }
